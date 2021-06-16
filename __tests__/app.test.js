29795638b051d560b0ebc2f5aca50027beca3362
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-15 18:56:51 -0700

    create profile working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,27 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('profile routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('create a profile for our user via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/profiles')
+      .send({
+        name: 'Cat',
+        favoriteCharacter: 'Hermes'
+      });
+
+    console.log(res.body);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id:'1',
+      name:'Cat',
+      favoriteCharacter: 'Hermes',
+      tagline: expect.any(String)
+    });
+  });
+
 });
